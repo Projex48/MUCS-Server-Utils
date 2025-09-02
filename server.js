3a2c6cc52278b65ea6851ac3be1c719001a0318b
Tristan Rush <145778062+Projex48@users.noreply.github.com>
2025-09-02 02:53:58 -0400

    Parallelized Fetches
    Parallelized fetches made to server status endpoints. Changed code on server to handle parallelization.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const {NodeSSH} = require('node-ssh')
+const { NodeSSH } = require('node-ssh')
+const cors = require('cors')
 require('dotenv').config()
-
-const ssh = new NodeSSH()
 
 const app = express()
 const PORT = 3000
@@ -10,8 +9,11 @@ const PORT = 3000
 const username = process.env.USER_NAME
 const keypath = `C:\\Users\\Tristan\\.ssh\\id_rsa`
 
+app.use(cors())
+
 app.get('/:server', async (req, res) => {
-	const {server} = req.params;
+	const ssh = new NodeSSH()
+	const { server } = req.params;
 	const address = "csciarch0" + server + ".millersville.edu"
 
 	await ssh.connect({
@@ -20,17 +22,15 @@ app.get('/:server', async (req, res) => {
 		privateKeyPath: keypath
 	})
 
-	console.log(`CS Arch Server ${server} Statistics: `)
-	ssh.execCommand(`top -b -n=1 | grep '%Cpu\\|MiB Mem'`).then(function(result) {
+	ssh.execCommand(`top -b -n=1 | grep '%Cpu\\|MiB Mem'`).then(function (result) {
 		var output = result.stdout;
 		var cpuUsage = Number(output.substring(output.indexOf(":") + 1, output.indexOf("us")).trim());
 		var memUsage = Number(output.substring(output.indexOf("free,") + 5, output.indexOf("used")).trim());
 		var memTotal = Number(output.substring(output.indexOf("Mem :") + 5, output.indexOf("total")).trim());
-		var memPct = memUsage/memTotal
-		memPct = (memPct.toFixed(2)) * 100
-		console.log("CPU Usage:", cpuUsage, "%")
-		console.log("Mem Usage:", memPct, "%")
-	})
+		var memPct = (memUsage / memTotal) * 100;
+		memPct = Number(memPct.toFixed(0));
+		res.status(200).json({ cpu: cpuUsage, mem: memPct });
+	}).catch(() => res.status(500))
 })
 
 app.listen(PORT)
